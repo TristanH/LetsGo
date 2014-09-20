02e1a2bf8d39b405f9d489bc4d33b38ba2de37ea
tristan <tristanhomsi@gmail.com>
2014-09-20 19:32:33 -0400

    integrate yelp api, overlay map, set up infrastructure

--- hackthenorth/urls.py
+++ hackthenorth/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = patterns('',
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^get_info/$', views.get_business_info, name='get_business_info'),
+
 )
